7f73ada112e619ea0d2c9fb4fb5a0ab21b76b67e
Alexandros Schillings <aschillings@gmail.com>
2018-06-04 12:07:43 +0100

    Fix for #3 Log some information about the device when a Debug wrapper is created

--- library/src/main/java/uk/co/alt236/bluetoothdebug/gatt/Validator.java
+++ library/src/main/java/uk/co/alt236/bluetoothdebug/gatt/Validator.java
@@ -2,7 +2,7 @@ package uk.co.alt236.bluetoothdebug.gatt;
 
 import android.bluetooth.BluetoothGattCallback;
 
-import uk.co.alt236.bluetoothdebug.reflection.OverrideValidator;
+import uk.co.alt236.bluetoothdebug.validation.OverrideValidator;
 
 final class Validator extends OverrideValidator<
         BluetoothGattCallback,
--- library/src/main/java/uk/co/alt236/bluetoothdebug/scanle/Validator.java
+++ library/src/main/java/uk/co/alt236/bluetoothdebug/scanle/Validator.java
@@ -2,7 +2,7 @@ package uk.co.alt236.bluetoothdebug.scanle;
 
 import android.bluetooth.le.ScanCallback;
 
-import uk.co.alt236.bluetoothdebug.reflection.OverrideValidator;
+import uk.co.alt236.bluetoothdebug.validation.OverrideValidator;
 
 final class Validator extends OverrideValidator<
         ScanCallback,
